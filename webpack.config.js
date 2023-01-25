2555188a7d01edbb6091b2d2557757c688a0e82b
Akshaya <rakshayaee@gmail.com>
2023-01-25 14:33:38 +0530

    Mfe2 exposes changes

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const moduleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+module.exports = {
+    mode: 'development',
+    devServer: {
+        port: '8082'
+    },
+    module: {
+        rules : [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                          ['@babel/preset-react', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.(css)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'css-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new moduleFederationPlugin({
+            name: 'mfe2',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './Mfe2Index': './src/App'
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+};
